d5054bf0b2e9dcdfcc9a7bc2f3079e7ad8d5c927
Vladimir Apraksin (OSV) <vladimir.apraksin.osv@fedex.com>
2023-03-21 11:57:09 -0400

    Inline styling.

--- src/index.js
+++ src/index.js
@@ -21,7 +21,14 @@ const Book = () => (
 
 const Image = () => <img src="./images/book-1.jpg" alt="Atomic Habits" />;
 const Title = () => <h2>Atomic Habits</h2>;
-const Author = () => <h4>James Clear</h4>;
+const Author = () => {
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlineHeadingStyles}>James Clear</h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
