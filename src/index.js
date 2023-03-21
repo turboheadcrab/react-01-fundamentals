2314c4eefd31413613b3136059285bdbf82c11db
turboheadcrab <turbo.headcrab.666@gmail.com>
2023-03-21 11:42:08 -0400

    Local images in public folder.

--- src/index.js
+++ src/index.js
@@ -19,12 +19,7 @@ const Book = () => (
   </article>
 );
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg"
-    alt="Atomic Habits"
-  />
-);
+const Image = () => <img src="./images/book-1.jpg" alt="Atomic Habits" />;
 const Title = () => <h2>Atomic Habits</h2>;
 const Author = () => <h4>James Clear</h4>;
 
