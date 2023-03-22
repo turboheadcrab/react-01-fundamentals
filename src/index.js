7944e61184216c04d40a4f2cd4a432d254526113
turboheadcrab <turbo.headcrab.666@gmail.com>
2023-03-22 01:28:15 -0400

    Use component props inside component function

--- src/index.js
+++ src/index.js
@@ -19,49 +19,23 @@ const books = [
 
 const BookList = () => (
   <section className="booklist">
-    <EventExamples />
     {books.map((book) => (
       <Book {...book} key={book.id} />
     ))}
   </section>
 );
 
-const EventExamples = () => (
-  <section>
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        console.log("form submitted");
-      }}
-    >
-      <h2>Typical Form</h2>
-      <input
-        type="text"
-        name="example"
-        onChange={(e) => {
-          console.log(e);
-          // e.target - element
-          console.log(`Input Name : ${e.target.name}`);
-          console.log(`Input Value : ${e.target.value}`);
-          // console.log('handle form input');
-        }}
-        style={{ margin: "1rem 0" }}
-      />
-      <button type="submit">submit</button>
-      <div>
-        <button onClick={() => alert("handle button click")}>click me</button>
-      </div>
-    </form>
-  </section>
-);
-
 const Book = (props) => {
   const { img, title, author } = props;
   // console.log(props);
+  const displayTitle = () => {
+    console.log(title);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>display title</button>
       <h4>{author}</h4>
     </article>
   );
