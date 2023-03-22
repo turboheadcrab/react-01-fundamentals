535a6db499fd9d8efcfb3d60e01f3b06f47f5137
turboheadcrab <turbo.headcrab.666@gmail.com>
2023-03-22 00:47:13 -0400

    Events - Fundamentals

--- src/index.js
+++ src/index.js
@@ -19,15 +19,39 @@ const books = [
 
 const BookList = () => (
   <section className="booklist">
+    <EventExamples />
     {books.map((book) => (
       <Book {...book} key={book.id} />
     ))}
   </section>
 );
 
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log("handle form input");
+  };
+  const handleButtonClick = () => {
+    alert("handle button click");
+  };
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
+  );
+};
+
 const Book = (props) => {
   const { img, title, author } = props;
-  console.log(props);
+  // console.log(props);
   return (
     <article className="book">
       <img src={img} alt={title} />
