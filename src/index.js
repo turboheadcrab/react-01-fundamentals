28c80e7cdee547c597e290aba594db280b5348ed
turboheadcrab <turbo.headcrab.666@gmail.com>
2023-03-22 00:29:07 -0400

    Iterating over a list inside a component and passing the entire object to it.

--- src/index.js
+++ src/index.js
@@ -7,20 +7,21 @@ const books = [
     author: "James Clear",
     title: "Atomic Habits",
     img: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81bGKUa1e0L.jpg",
+    id: 1,
   },
   {
     author: "Karl Marx",
     title: "Das Kapital",
     img: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/41mfCeuOk1L.jpg",
+    id: 2,
   },
 ];
 
 const BookList = () => (
   <section className="booklist">
-    {books.map((book) => {
-      const { img, title, author } = book;
-      return <Book img={img} title={title} author={author} />;
-    })}
+    {books.map((book) => (
+      <Book {...book} key={book.id} />
+    ))}
   </section>
 );
 
